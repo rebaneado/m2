354aac5c409241f3aa3248ada3d3ffd6ce25a48a
Hugo <hamolinagomez@gmail.com>
2023-02-04 09:14:19 -0500

    Added code internally to get host name snd run c program

--- 1/hello.cpp
+++ 1/hello.cpp
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <unistd.h>
+
 int main () {
-  printf("Hostname comand: 8986fc7dd4ec");
+  // //printf("Hostname comand: 8986fc7dd4ec");
+  // 15
+  // printf(gethostname);
+  // //char a = std::stof(gethostname);
+  // return 0;
+
+
+
+
+
+  char hostbuffer[256];
+  int hostname;
+
+  // To retrieve hostname
+  hostname = gethostname(hostbuffer, sizeof(hostbuffer));
+  
+  printf("Hostname: %s\n", hostbuffer);
   return 0;
 
   
