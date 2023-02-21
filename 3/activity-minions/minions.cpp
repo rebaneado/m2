955684a11e7feae36f4478b2c140d70aaa9b7c0c
rebaneado <hamolinagomez@gmail.com>
2023-02-21 13:58:17 -0500

    finished the threading of ./minions #number to take input and create thread per loop

--- 3/activity-minions/minions.cpp
+++ 3/activity-minions/minions.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
 #include <thread>
-
+#include <vector>
 
 void f (int number){
   std::cout <<"Hello! I am Minion: "<<number<<"\n";
 }
-
-
 int main (int argc, char** argv) {
   std::vector<std::thread> mythreads;
   int inputNumber = std::stof(argv[1]);
-
 
   if (argc < 2) {
     std::cerr<<"usage: "<<argv[0]<<" <nbminions>\n";
     return -1;
   }
-  std::cout<<"This is the input number: "<<inputNumber<<"/n";
-
+  std::cout<<"This is the input number: "<<inputNumber<<"\n";
   for (int i = 0; i<inputNumber; i++){
     std::thread mythread(f,i);
     mythreads.push_back(std::move(mythread));
@@ -31,16 +27,11 @@ int main (int argc, char** argv) {
       t.join();
     }
     else
-      std::cout<<"t is not joinable\n";
-    
+      std::cout<<"t is not joinable/n";
   }
-  
-  
+  std::cout<<"Hello Minions! I am the Overload!";
   return 0;
 }
-
-
-
 
   // for (int i = 0; i < argc; ++i){
   //   std::cout << argv[i] << "\n";
