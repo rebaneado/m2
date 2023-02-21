18a7c1f93ef890720bd03f56cc5ccc56d583be80
rebaneado <hamolinagomez@gmail.com>
2023-02-21 13:32:31 -0500

    added a c++ threading module

--- 1/hello.cpp
+++ 1/hello.cpp
@@ -1,25 +1,21 @@
-#include <stdio.h>
-#include <unistd.h>
 
-int main () {
-  // //printf("Hostname comand: 8986fc7dd4ec");
-  // 15
-  // printf(gethostname);
-  // //char a = std::stof(gethostname);
-  // return 0;
-
-
-
-
-
-  char hostbuffer[256];
-  int hostname;
-
-  // To retrieve hostname
-  hostname = gethostname(hostbuffer, sizeof(hostbuffer));
-  
-  printf("Hostname: %s\n", hostbuffer);
-  return 0;
-
-  
+double expBySquaring(double x, int n){
+  if (n == 0) return 1;
+  if (n == 1) return x;
+  double base;
+  int exp;
+  double mult;
+  if (n%2 == 0) {
+    base = x;
+    exp=n/2;
+    mult=1;
+  }
+  else {
+    base=x*x;
+    exp=(n-1)/2;
+    mult=x;
+  }// recursive 
+  double res = expBySquaring(base,exp); //line call  -- split this up... 
+  //the function call should break up into 3 diff nodes at least 
+  return res*mult;//call task 
 }
