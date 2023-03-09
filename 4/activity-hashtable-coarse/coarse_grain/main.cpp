b139b2cd8df8789111f14f9948cad9b5a30ae4b9
Hugo <hamolinagomez@gmail.com>
2023-03-09 14:03:32 -0500

    got it to work without the Mutexes

--- 4/activity-hashtable-coarse/coarse_grain/main.cpp
+++ 4/activity-hashtable-coarse/coarse_grain/main.cpp
@@ -49,13 +49,13 @@ std::vector<std::vector<std::string> > tokenizeLyrics(const std::vector<std::str
 }
 
 
-void function(std::vector<std::string>& filecontent, MyHashtable<std::string, int>& ht , std::mutex& mut){
+void function(std::vector<std::string>& filecontent, Dictionary<std::string, int>& dict, std::mutex& mut ){
         for (auto & w : filecontent) {
-          mut.lock();
-          int count = ht.get(w);
+          //mut.lock();
+          int count = dict.get(w);
           ++count;
-          ht.set(w, count);
-          mut.unlock();
+          dict.set(w, count);
+          //mut.unlock();
         }
 
 }
@@ -87,21 +87,22 @@ int main(int argc, char **argv)
 
 
    // write code here
-   auto start =std::chrono::steady_clock::now();
+  // auto start =std::chrono::steady_clock::now();
 
 //below make vector of mutexes
-  std::vector<std::thread> threadgroup;
+  std::vector<std::thread> mythreads;
+
   int val = 0;
   std::mutex mut;
 
-
+//!here is the issue
   for(int i = 0; i< files.size(); i++){
-    std::thread mythread(function, std::ref (wordmap[i]), std::ref (ht), std::ref (mut));
-    threadgroup.push_back(std::move(mythread));
+    std::thread mythread(function, std::ref (wordmap[i]), std::ref (dict), std::ref (mut)); //
+    mythreads.push_back(std::move(mythread));
 
   }
 
-  for (auto & t : threadgroup)
+  for (auto & t : mythreads)
     {
       if (t.joinable())
       {
@@ -112,8 +113,8 @@ int main(int argc, char **argv)
     }
 
 
-  auto stop = std::chrono::steady_clock::now();
-  std::chrono::duration<double> time_elapsed = stop-start;
+  // auto stop = std::chrono::steady_clock::now();
+  // std::chrono::duration<double> time_elapsed = stop-start;
 
    
   // Check Hash Table Values 
